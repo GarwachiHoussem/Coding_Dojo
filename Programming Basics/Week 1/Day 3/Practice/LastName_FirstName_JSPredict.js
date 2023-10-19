6cd0a9ac419982a55ad21723d3d8812f2a0bee16
GarwachiHoussem <garwachi.houssem@gmail.com>
2023-10-19 05:11:36 +0100

    day3 practice

---
+++ Programming Basics/Week 1/Day 3/Practice/LastName_FirstName_JSPredict.js
@@ -0,0 +1,19 @@
+// function myBirthYearFunc(){
+//     console.log("I was born in" + 1995);
+// }
+// myBirthYearFunc()
+// myBirthYearFunc();
+// function myBirthYearFunc(birthYearInput){
+//     console.log("I was born in " + birthYearInput);
+// }
+// myBirthYearFunc(1995);
+
+function add(num1, num2){
+    console.log("Summing Numbers!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+add(10, 20);
+
