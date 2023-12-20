7f31dca5cf45e9bb15faa7bd2b15366391aaa81c
GarwachiHoussem <garwachi.houssem@gmail.com>
2023-12-20 18:56:35 +0100

    commit

---
+++ MERNStack/week_1/Day_3/Core/first_jsx/react-state/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { MyFirstReact } from './components/MyFirstReact';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyFirstReact/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ MERNStack/week_1/Day_3/Core/first_jsx/react-state/src/components/MyFirstReact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const MyFirstReact = () => {
+  return (
+    <div><h1>MY name is Houssem</h1>
+    <ul>
+    <li>Learn React</li>
+    <li>Climb mt everest</li>
+    <li>Run a Marathon</li>
+    <li>Feed the dogs</li></ul>
+    
+    </div>
+  )
+}
+
