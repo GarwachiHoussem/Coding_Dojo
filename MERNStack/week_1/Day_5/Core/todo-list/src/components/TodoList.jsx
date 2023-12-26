14469d0e109961386f7d847f84ec2efa6cefdb61
GarwachiHoussem <garwachi.houssem@gmail.com>
2023-12-26 03:50:49 +0100

    commit

--- MERNStack/week_1/Day_5/Core/todo-list/src/components/TodoList.jsx
+++ MERNStack/week_1/Day_5/Core/todo-list/src/components/TodoList.jsx
@@ -2,49 +2,49 @@ import React, { useEffect, useState } from 'react';
 import InputForm from './InputForm';
 
 export default function TodoList() {
-  const [todoList, setTodoList] = useState([]);
-  
-  useEffect(() => {
-    console.log('use effect')
-    const items = JSON.parse(localStorage.getItem('todoList'));
-    items && setTodoList(items);
-  }, []);
+    const [todoList, setTodoList] = useState([]);
 
-  const addNote = (note) => {
-    const newNote = {note, checked: true};
-    setTodoHelper([...todoList].concat(newNote));
-  }
+    useEffect(() => {
+        console.log('use effect')
+        const items = JSON.parse(localStorage.getItem('todoList'));
+        items && setTodoList(items);
+    }, []);
 
-  const onCheck = (index) => {
-    const newNotes = [...todoList];
-    newNotes[index].checked = !newNotes[index].checked;
-    setTodoHelper(newNotes);
-  }
-  const deleteNote = (index) => {
-    const newNotes = [...todoList];
-    newNotes.splice(index, 1);
-    setTodoHelper(newNotes);
-  };
+    const addNote = (note) => {
+        const newNote = { note, checked: true };
+        setTodoHelper([...todoList].concat(newNote));
+    }
 
-  const setTodoHelper = (newNotes) => {
-    setTodoList(newNotes);
-    localStorage.setItem('todoList', JSON.stringify(newNotes));
-  }
+    const onCheck = (index) => {
+        const newNotes = [...todoList];
+        newNotes[index].checked = !newNotes[index].checked;
+        setTodoHelper(newNotes);
+    }
+    const deleteNote = (index) => {
+        const newNotes = [...todoList];
+        newNotes.splice(index, 1);
+        setTodoHelper(newNotes);
+    };
 
-  return (
-    <>
-      <InputForm addNote = {addNote} />
-      {todoList.map((todo, index) => {
-        const note = todo.checked ? <p><s>{todo.note}</s></p> : <p>{todo.note}</p>;
+    const setTodoHelper = (newNotes) => {
+        setTodoList(newNotes);
+        localStorage.setItem('todoList', JSON.stringify(newNotes));
+    }
 
-        return (
-          <div className="form-check" key={index}>
-            <label className="form-check-label">{note}</label>
-            <input type="checkbox" checked={todo.checked} onChange={() => onCheck(index)}></input>
-            <button type="button" className="btn btn-danger" onClick={ () => deleteNote(index) }>Delete</button>
-          </div>
-        )
-      })}
-    </>
-  );
+    return (
+        <>
+            <InputForm addNote={addNote} />
+            {todoList.map((todo, index) => {
+                const note = todo.checked ? <p><s>{todo.note}</s></p> : <p>{todo.note}</p>;
+
+                return (
+                    <div className="form-check" key={index}>
+                        <label className="form-check-label">{note}</label>
+                        <input type="checkbox" checked={todo.checked} onChange={() => onCheck(index)}></input>
+                        <button type="button" className="btn btn-danger" onClick={() => deleteNote(index)}>Delete</button>
+                    </div>
+                )
+            })}
+        </>
+    );
 }
