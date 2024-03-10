7cf2049d3bd5c7a5a97c2c9fd40a3120ee54b87b
GarwachiHoussem <garwachi.houssem@gmail.com>
2024-03-10 16:40:12 +0100

    commit

---
+++ Java/Your_Own_Java_Program/My_own.java
@@ -0,0 +1,11 @@
+public class My_own {
+    public static void main(String[] args){
+        String name= " I'm Houssem Garwachi ";
+        int age= 28;
+        String hometown= "My hometown is Tunisia, TN";
+            System.out.println(name);
+            System.out.println(age);
+            System.out.println(hometown);
+    }
+    
+}
