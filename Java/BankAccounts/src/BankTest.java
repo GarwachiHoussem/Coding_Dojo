763cd68039316b423641c83f1f398a551ca7ab2e
GarwachiHoussem <garwachi.houssem@gmail.com>
2024-03-18 16:17:15 +0100

    commit

--- Java/BankAccounts/src/BankTest.java
+++ Java/BankAccounts/src/BankTest.java
@@ -3,21 +3,24 @@ public class BankTest {
     {
         //Create a new Bank Account with an initial balance of 100.
         //Instantiate the bank account object and call deposit method to add money into it, then withdraw some amount from it.
-        BankAccount account = new BankAccount(1000,25100);
-        System.out.println(account.totalBalance());    
+    	 BankAccount account = new BankAccount(0,25100);
+//        BankAccount account1 = new BankAccount(1000,25100);
+        System.out.println("TotalBalance"+ account.totalBalance());    
         System.out.println("++++++++++++++++");
         account.depositMoney(25,"checking");
-        System.out.println(account.getCheckingBalance());
+        System.out.println("Deposit money"+ account.getCheckingBalance());
         System.out.println("++++++++++++++++");
+        System.out.println("TotalBalance"+ account.totalBalance());
 
         account.withdrawMoney(75,"saving");
-        System.out.println(account.getSavingBalance());
+        System.out.println("Withdraw money"+ account.getSavingBalance());
+        System.out.println("TotalBalance"+ account.totalBalance());
 
         System.out.println("++++++++++++++++");
 
-        System.out.println(BankAccount.getNumberOfAccounts());     
+        System.out.println("Number of Account "+ BankAccount.getNumberOfAccounts());     
             System.out.println(BankAccount.getTotalAmount());    
    
-        System.out.println(account.totalBalance());    
+        System.out.println("Total Balance" + account.totalBalance());    
     }
 }
