c85946924dbbb30d08d67695cb2b0521490d76fb
GarwachiHoussem <garwachi.houssem@gmail.com>
2024-03-28 21:27:46 +0100

    commit

---
+++ Java/Daikichi_Routes/src/main/java/com/houssem/Daikichi_Routes/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.houssem.Daikichi_Routes;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DaikichiRoutesApplication.class);
+	}
+
+}
